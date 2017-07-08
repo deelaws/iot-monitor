fca88324e2876be92cd4647b7d7e459aa43b2d69
Waleed <deelaws89@gmail.com>
2017-07-07 17:28:42 -0700

    Add invalid input handler.

--- mod_app/app.py
+++ mod_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from mod_app.config import config_map
 
@@ -13,11 +13,20 @@ def create_app(config_name):
     db.init_app(app)
 
     # Register Blueprints
+    from mod_sensor.views import mod_sensor
+    from mod_sensor.exceptions import InvalidInput
+    app.register_blueprint(mod_sensor)
 
     @app.route("/")
     def hello():
         return "Hello World"
 
-    print(app.url_map)
+    @app.errorhandler(InvalidInput)
+    def handle_invalid_usage(error):
+        response = jsonify(error.to_dict())
+        response.status_code = error.status_code
+        return response
+
+    #print(app.url_map)
 
     return app
